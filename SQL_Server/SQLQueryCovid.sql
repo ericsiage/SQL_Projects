6fcbf70ea159b3aa4e9f8b99eb7224175e761c08
Eric Agustin Gutierrez <75638143+ericsiage@users.noreply.github.com>
2022-02-27 15:30:05 -0300

    Update SQLQueryCovid.sql

--- SQL_Server/SQLQueryCovid.sql
+++ SQL_Server/SQLQueryCovid.sql
@@ -1,39 +1,43 @@
---Select VC.iso_code, VC.location, VC.daily_vaccinations,
---DC.iso_code, DC.location, DC.new_deaths
---from VaccinationsCovid AS VC
---inner join DeathsCovid AS DC
---ON VC.location = DC.location
-
---ORDER BY VC.daily_vaccinations DESC
-
 --Select Location, date, total_cases, new_cases, total_deaths, population
 --From ProjectPortfolio..DeathsCovid as DC
 --order by 1,2
 
 
--- Casos totales vs muertes totales
--- muestra el porcentaje de muertes por casos
---Select location, date, total_deaths, total_cases, (total_deaths/total_cases)*100 as percentage_deaths_by_cases 
---from ProjectPortfolio..DeathsCovid as DC
---order by 1,2 DESC
 
 
---muestra el porcentaje de casos por poblaci�n
-select location, date, population, total_cases, (total_cases/population)*100 as percentage_cases_by_population
-from ProjectPortfolio..DeathsCovid
-where location like '%uruguay%'
-order by 1,2 DESC
+--muestra el porcentaje de muertes por población de cada pais
 
 
---vemos la tasa de infectados por pais
-select location, population, max(total_cases) as max_cases, max((total_cases/population))*100 as percentage_cases_by_country
+Select location, population, sum(new_deaths) as total_d, sum((new_deaths)/population)*100 as percentage_deaths_by_country
 from ProjectPortfolio..DeathsCovid
 group by location, population
-order by 1,2
+order by percentage_deaths_by_country desc
 
 
---Porcentaje de muertes con vacunas.
---select DC.location, DC.date, DC.population, VC.total_vaccinations, DC.total_deaths, (DC.total_deaths/VC.total_vaccinations)*100 as percentage_deaths_with_vaccines
---from ProjectPortfolio..DeathsCovid AS DC
---inner join ProjectPortfolio..VaccinationsCovid as VC
---on DC.location = VC.location
+
+
+--Vemos la tasa de infectados por pais
+
+select location, population, sum(new_cases) as Total_Cases, sum((new_cases)/population)*100 as percentage_cases_by_country
+from ProjectPortfolio..DeathsCovid
+group by location, population
+order by percentage_cases_by_country desc
+
+
+
+-- Relacion entre vacunas totales y muertes totales por pais
+
+Select VC.location, sum(VC.daily_vaccinations) as total_vac, sum(DC.new_deaths) as total_death
+from ProjectPortfolio..VaccinationsCovid AS VC
+inner join ProjectPortfolio..DeathsCovid AS DC ON VC.location = DC.location
+group by VC.location
+order by total_vac desc
+
+
+
+
+-- Muestra el porcentaje de muertes diario en uruguay en el año 2022
+
+Select location, date, total_deaths, total_cases, (total_deaths/total_cases)*100 as percentage_deaths_by_cases 
+from ProjectPortfolio..DeathsCovid as DC
+where location = 'Uruguay' AND Date like '%2022%'
